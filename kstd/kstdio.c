7422c856ddb1675388dc9de0b1c74d75b8a205f0
tzyvoski <OxyGen05@outlook.it>
2022-02-23 10:55:26 +0100

    Updated stdio

--- kstd/kstdio.c
+++ kstd/kstdio.c
@@ -10,22 +10,26 @@ void kprintf(const char* __fmt, ...) {
     va_list _arguments;
     va_start(_arguments, __fmt);
 
-    while (*_ptr != 0) {
-        if (*_ptr == '%' && *(_ptr + 1) != ' ') {
-            switch (*(_ptr + 1)) {
-                case 'u': { kernel_text_print_str((char*)(uitoa(va_arg(_arguments, uint64_t)))); } break;
-                
-                case 'd':
-                case 'i': { kernel_text_print_str((char*)(itoa(va_arg(_arguments, int64_t))));   } break;
+    while (*_ptr) {
+        switch (*_ptr) {
+            case '%': {
+                switch (*(_ptr + 1)) {
+                    case 'u': { kernel_text_print_str((char*)(uitoa(va_arg(_arguments, uint64_t)))); } break;
+                    
+                    case 'd':
+                    case 'i': { kernel_text_print_str((char*)(itoa(va_arg(_arguments, int64_t))));   } break;
 
-                case 'c': { kernel_text_print_char((char)(va_arg(_arguments, signed)));                       } break;
+                    case 'c': { kernel_text_print_char((char)(va_arg(_arguments, signed)));                       } break;
+                }
+
+                _ptr++;
+                break;
             }
 
-            _ptr++;
+            default:
+                kernel_text_print_char(*_ptr);
+                break;
         }
-        
-        else
-        kernel_text_print_char(*_ptr);
 
         _ptr++;
     }
