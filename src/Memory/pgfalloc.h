0bbae27ed67801c7d0275b1ace8db15d33c65bf1
tzyvoski <OxyGen05@outlook.it>
2022-02-24 22:43:17 +0100

    Fixed ordering in pgf header file

--- src/Memory/pgfalloc.h
+++ src/Memory/pgfalloc.h
@@ -12,8 +12,8 @@
     void            kernel_allocator_initialize         ();
     void            kernel_allocator_free               (void* __address, uint64_t __pagecount);
     void            kernel_allocator_lock               (void* __address, uint64_t __pagecount);
-    void            kernel_allocator_reserve            (void* __address, uint64_t __pagecount);
     void            kernel_allocator_unreserve          (void* __address, uint64_t __pagecount);
+    void            kernel_allocator_reserve            (void* __address, uint64_t __pagecount);
 
     void            kernel_allocator_info_get           (uint64_t* __freemem, uint64_t* __usedmem, uint64_t* __reservedmem);
     void*           kernel_allocator_page_new           ();
