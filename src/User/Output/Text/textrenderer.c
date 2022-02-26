4f903bd85f0f9801998b1b17084dec18b3c40d46
tzyvoski <OxyGen05@outlook.it>
2022-02-26 21:10:30 +0100

    Updated Text Renderer - No bloat

--- src/User/Output/Text/textrenderer.c
+++ src/User/Output/Text/textrenderer.c
@@ -4,176 +4,163 @@
 
 static bmpfont_t* font;
 
-static uint32_t columns;
-static uint32_t rows;
-static uint32_t x0, y0;
+static uint32_t numColumns;
+static uint32_t numRows;
 
-static uint32_t color;
-static uint32_t backcolor;
-static point_t  cursor;
+static uint32_t foregroundColor;
+static uint32_t backgroundColor;
 
-static bool_t  textInitialized;
+static point_t startPosition;
+static point_t endPosition;
+static point_t curPosition;
+
+static bool_t  trendrInitialized;
 
 
-static void __putchar__(bmpfont_t* __font, uint32_t __color, char __char, uint32_t __xoff, uint32_t __yoff) {
-    char* _font_ptr = __font->_Buffer + (__char * __font->_Header->_CharSize);
+static void __initialize__(uint32_t __color, uint32_t __backcolor, uint64_t __xoff, uint64_t __yoff) {
+    uint64_t _buff_w = kernel_kdd_fbo_get()->_Width;
+    uint64_t _buff_h = kernel_kdd_fbo_get()->_Height;
 
-    for (uint64_t _y = __yoff; _y < __yoff + 16; _y++) {
-        for (uint64_t _x = __xoff; _x < __xoff + 8; _x++)
-            if ((*_font_ptr & (0b10000000 >> (_x - __xoff))) > 0)
-                kernel_kdd_pxcolor_set(_x, _y, __color);
+    foregroundColor = __color;
+    backgroundColor = __backcolor;
+    
+    startPosition = (point_t) {
+        .x = __xoff,
+        .y = __yoff
+    }; 
+
+    endPosition = (point_t) {
+        .x = _buff_w - (_buff_w %  8),
+        .y = _buff_h - (_buff_h % 16)
+    };
+
+    curPosition = startPosition;
+
+    numColumns = endPosition.x / 8;
+    numRows    = endPosition.y / 16;
+
+    kernel_kdd_fbo_clear(backgroundColor);
+}
+
+static void __putchar__(char __char) {
+    char* _font_ptr = font->_Buffer + (__char * font->_Header->_CharSize);
+
+    uint64_t _start_x = curPosition.x;
+    uint64_t _start_y = curPosition.y;
+
+    uint64_t _end_x = _start_x + 8,
+             _end_y = _start_y + 16;
+
+    for (uint64_t _y = _start_y; _y < _end_y; _y++) {
+        for (uint64_t _x = _start_x; _x < _end_x; _x++) {
+            kernel_kdd_pxcolor_set(
+                _x, _y, 
+                ((*_font_ptr & (0b10000000 >> (_x - _start_x))) > 0) ? foregroundColor :
+                                                                       backgroundColor
+            );
+        }
 
         _font_ptr++;
     }
 }
 
 static void __newline__() {
-    cursor.x  = x0;
-    cursor.y += 16;
+    curPosition.x  = startPosition.x;
+    curPosition.y += 16;
 
-    SOFTASSERT(!(cursor.y < kernel_kdd_fbo_get()->_Height), RETVOID);
+    SOFTASSERT(!(curPosition.y < endPosition.y), RETVOID);
 
     kernel_text_scroll_up(1);
 }
 
 void kernel_text_scroll_up(uint32_t __lines) {
-    SOFTASSERT(textInitialized, RETVOID);
+    SOFTASSERT(trendrInitialized, RETVOID);
     SOFTASSERT(__lines != 0, RETVOID);
-    SOFTASSERT(__lines < rows, RETVOID);
+    SOFTASSERT(__lines < numRows, RETVOID);
     
-    for (uint64_t y = __lines * 16; y < kernel_kdd_fbo_get()->_Height; y++) {
-        for (uint64_t x = x0; x < kernel_kdd_fbo_get()->_PixelsPerScanLine; x++) {
+    for (uint64_t _y = __lines * 16; _y < endPosition.y; _y++) {
+        for (uint64_t _x = startPosition.x; _x < kernel_kdd_fbo_get()->_PixelsPerScanLine; _x++) {
             kernel_kdd_pxcolor_set(
-                x, y - __lines * 16,
-                kernel_kdd_pxcolor_get(x, y)
+                _x, _y - __lines * 16,
+                kernel_kdd_pxcolor_get(_x, _y)
             );
         }
     }
 
-    for (uint64_t y = cursor.y - (16 * __lines); y < kernel_kdd_fbo_get()->_Height; y++) {
-        for (uint64_t x = x0; x < kernel_kdd_fbo_get()->_PixelsPerScanLine; x++) {
-            kernel_kdd_pxcolor_set(
-                x, y,
-                backcolor
-            );
-        }
-    }
+    for (uint64_t _l = 0; _l < __lines; _l++)
+        kernel_text_line_clear(curPosition.y / 16 - _l);
 
-    cursor.y -= 16 * __lines;
+    curPosition.y -= 16 * __lines;
 }
 
-void kernel_text_scroll_down(uint32_t __lines) {
-    SOFTASSERT(textInitialized, RETVOID);
-    SOFTASSERT(__lines != 0, RETVOID);
-    SOFTASSERT(__lines < rows, RETVOID);
+void kernel_text_line_clear(uint32_t __line) {
+    SOFTASSERT(trendrInitialized, RETVOID);
 
-    for (uint64_t y = __lines * 16; y < kernel_kdd_fbo_get()->_Height; y++) {
-        for (uint64_t x = x0; x < kernel_kdd_fbo_get()->_PixelsPerScanLine; x++) {
+    for (uint64_t _y = __line * 16 - 16; _y < __line * 16; _y++) {
+        for (uint64_t _x = startPosition.x; _x < endPosition.x; _x++) {
             kernel_kdd_pxcolor_set(
-                x, y + __lines * 16,
-                kernel_kdd_pxcolor_get(x, y)
+                _x, _y,
+                backgroundColor
             );
         }
     }
-
-    for (uint64_t y = 0; y < __lines * 16; y++) {
-        for (uint64_t x = x0; x < kernel_kdd_fbo_get()->_PixelsPerScanLine; x++) {
-            kernel_kdd_pxcolor_set(
-                x, y,
-                backcolor
-            );
-        }
-    }
-}
-
-void kernel_text_clear_line(uint32_t __line) {
-    SOFTASSERT(textInitialized, RETVOID);
 }
 
 void kernel_text_print_char(char __char) {
-    SOFTASSERT(textInitialized, RETVOID);
+    SOFTASSERT(trendrInitialized, RETVOID);
     SOFTASSERT(__char != 0, RETVOID);
 
-    if (__char == '\n') {
-        __newline__();
-        return;
+    switch (__char) {
+        case '\n': __newline__(); return;
+        case '\t': return;
     }
 
-    if (cursor.x == kernel_kdd_fbo_get()->_PixelsPerScanLine)
+    if (curPosition.x == endPosition.x)
         __newline__();
     
-    __putchar__(
-        font, color, __char,
-        cursor.x, cursor.y
-    );
-
-    cursor.x += 8;
+    __putchar__(__char);
+    curPosition.x += 8;
 }
 
 void kernel_text_print_str(char* __str) {
-    SOFTASSERT(textInitialized, RETVOID);
+    SOFTASSERT(trendrInitialized, RETVOID);
     
-    char* chr = __str;
+    char* _chr = __str;
 
-    while (*chr != 0) {
-        kernel_text_print_char(*chr);
-        *chr++;
+    while (*_chr != 0) {
+        kernel_text_print_char(*_chr);
+        *_chr++;
     }
 }
 
-void kernel_text_set_color(uint32_t __color, uint32_t __backcolor) {
-    SOFTASSERT(textInitialized, RETVOID);
+void kernel_text_info_set(uint32_t __color, uint32_t __backcolor) {
+    SOFTASSERT(trendrInitialized, RETVOID);
     
-    color     = __color;
-    backcolor = __backcolor; 
+    foregroundColor = __color;
+    backgroundColor = __backcolor; 
 }
 
 void kernel_text_initialize(uint32_t __color, uint32_t __backcolor, uint32_t __xoff, uint32_t __yoff, bmpfont_t* __font) {
-    SOFTASSERT(!textInitialized, RETVOID);
+    SOFTASSERT(!trendrInitialized, RETVOID);
     
-    color     = __color;
-    backcolor = __backcolor;
-    x0        = __xoff;
-    y0        = __yoff;
-    font      = __font;
+    __initialize__(__color, __backcolor, __xoff, __yoff);
+    font = __font;
 
-    cursor.x = x0;
-    cursor.y = y0;
-
-    columns = kernel_kdd_fbo_get()->_PixelsPerScanLine / 8;
-    rows    = kernel_kdd_fbo_get()->_Height / 16;
-
-    kernel_kdd_fbo_clear(backcolor);
-
-    textInitialized = TRUE;
+    trendrInitialized = TRUE;
 }
 
 void kernel_text_reinitialize(uint32_t __color, uint32_t __backcolor, uint32_t __xoff, uint32_t __yoff) {
-    SOFTASSERT(textInitialized, RETVOID);
-
-    color     = __color;
-    backcolor = __backcolor;
-    x0        = __xoff;
-    y0        = __yoff; 
-
-    cursor.x  = x0;
-    cursor.y  = y0;
-
-    kernel_kdd_fbo_clear(backcolor);
+    SOFTASSERT(trendrInitialized, RETVOID);
+    __initialize__(__color, __backcolor, __xoff, __yoff);
 }
 
-void kernel_text_move_caret(int64_t __xoff, int64_t __yoff) {
-    cursor.x += __xoff * 16;
-    cursor.y += __yoff * 16;
-}
-
-void kernel_text_get_handles(uint32_t* __color, uint32_t* __backcolor, uint32_t* __xoff, uint32_t* __yoff, bmpfont_t** __font) {
-    SOFTASSERT(textInitialized, RETVOID);
+void kernel_text_info_get(uint32_t* __color, uint32_t* __backcolor, uint32_t* __xoff, uint32_t* __yoff, bmpfont_t** __font) {
+    SOFTASSERT(trendrInitialized, RETVOID);
     
-    ARGRET(__color, color);
-    ARGRET(__backcolor, backcolor);
+    ARGRET(__color, foregroundColor);
+    ARGRET(__backcolor, backgroundColor);
     
-    ARGRET(__xoff, cursor.x);
-    ARGRET(__yoff, cursor.y);
+    ARGRET(__xoff, curPosition.x);
+    ARGRET(__yoff, curPosition.y);
     ARGRET(__font, font);
 }
--- src/User/Output/Text/textrenderer.h
+++ src/User/Output/Text/textrenderer.h
@@ -5,18 +5,19 @@
     #include <kerntypes.h>
 
 
-    void kernel_text_print_char    (char __char);
-    void kernel_text_print_str     (char* __str);
+    /************************************************************************************************************************************************
+    RET TYPE        FUNCTION NAME                 FUNCTION ARGUMENTS
+    ************************************************************************************************************************************************/
+    void            kernel_text_print_char        (char __char);
+    void            kernel_text_print_str         (char* __str);
 
-    void kernel_text_move_caret    (int64_t __xoff, int64_t __yoff);
-    void kernel_text_get_handles   (uint32_t* __color, uint32_t* __backcolor, uint32_t* __xoff, uint32_t* __yoff, bmpfont_t** __font);
-    void kernel_text_initialize    (uint32_t __color, uint32_t __backcolor, uint32_t __xoff, uint32_t __yoff, bmpfont_t* __font); 
-    void kernel_text_reinitialize  (uint32_t __color, uint32_t __backcolor, uint32_t __xoff, uint32_t __yoff);
+    void            kernel_text_info_get          (uint32_t* __color, uint32_t* __backcolor, uint32_t* __xoff, uint32_t* __yoff, bmpfont_t** __font);
+    void            kernel_text_initialize        (uint32_t __color, uint32_t __backcolor, uint32_t __xoff, uint32_t __yoff, bmpfont_t* __font); 
+    void            kernel_text_reinitialize      (uint32_t __color, uint32_t __backcolor, uint32_t __xoff, uint32_t __yoff);
 
-    void kernel_text_scroll_up     (uint32_t __lines);
-    void kernel_text_scroll_down   (uint32_t __lines);
+    void            kernel_text_scroll_up         (uint32_t __lines);
 
-    void kernel_text_clear_line    (uint32_t __line);
-    void kernel_text_set_color     (uint32_t __color, uint32_t __backcolor);
+    void            kernel_text_line_clear        (uint32_t __line);
+    void            kernel_text_info_set          (uint32_t __color, uint32_t __backcolor);
 
 #endif
