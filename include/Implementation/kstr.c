c5bf550e343de19a58ca5b7033eab3a9fc23a1f2
tzyvoski <OxyGen05@outlook.it>
2022-03-11 09:47:54 +0100

    Saved a little bit of memory

--- include/Implementation/kstr.c
+++ include/Implementation/kstr.c
@@ -1,8 +1,7 @@
 #include <kstr.h>
 
 
-static char uintOutput[24];
-static char intOutput[24];
+static char kstrOutput[24];
 
 
 const char* uitoa(uint64_t __val) {
@@ -18,16 +17,16 @@ const char* uitoa(uint64_t __val) {
     while (_idx < _size) {
         uint8_t _rem              = __val % 10;
         __val                    /= 10;
-        uintOutput[_size - _idx] = _rem + '0';
+        kstrOutput[_size - _idx] = _rem + '0';
         _idx++;
     }
 
     uint8_t _rem              = __val % 10;
     __val                    /= 10;
-    uintOutput[_size - _idx] = _rem + '0';
-    uintOutput[_size + 1]    = 0;
+    kstrOutput[_size - _idx] = _rem + '0';
+    kstrOutput[_size + 1]    = 0;
     
-    return uintOutput;
+    return kstrOutput;
 }
 
 const char* itoa(int64_t __val) {
@@ -37,7 +36,7 @@ const char* itoa(int64_t __val) {
     uint8_t  _is_negative = 0;
 
     _is_negative = __val < 0;
-    intOutput[0] = (__val < 0) ? '-' : '+';
+    kstrOutput[0] = (__val < 0) ? '-' : '+';
     __val        = kabs(__val);
 
     while (__val / _size_test > 0) {
@@ -48,14 +47,14 @@ const char* itoa(int64_t __val) {
     while (_idx < _size) {
         uint8_t _rem                             = __val % 10;
         __val                                   /= 10;
-        intOutput[_is_negative + _size - _idx]  = _rem + '0';
+        kstrOutput[_is_negative + _size - _idx]  = _rem + '0';
         _idx++;
     }
 
     uint8_t _rem                             = __val % 10;
     __val                                   /= 10;
-    intOutput[_is_negative + _size - _idx] = _rem + '0';
-    intOutput[_is_negative + _size + 1]    = 0;
+    kstrOutput[_is_negative + _size - _idx]  = _rem + '0';
+    kstrOutput[_is_negative + _size + 1]     = 0;
     
-    return intOutput;
+    return kstrOutput;
 }
