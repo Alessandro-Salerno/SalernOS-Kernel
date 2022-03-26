1f2f66319c9f85a09a492c2bf5fc707a316ee585
tzyvoski <67583725+tzyvoski@users.noreply.github.com>
2022-03-26 17:15:36 +0100

    Updated to support new SEB changes

--- src/kernel.c
+++ src/kernel.c
@@ -22,9 +22,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <kdebug.h>
 
 
-void kernel_main(boot_t __bootinfo) {
-    uint8_t _seb_minver_day   = __bootinfo._SEBMinorVersion ^ ((__bootinfo._SEBMinorVersion >> 8) << 8);
-    uint8_t _seb_minver_month = (__bootinfo._SEBMinorVersion ^ _seb_minver_day) >> 8;
+void kernel_main(boot_t* __bootinfo) {
+    uint8_t _seb_minver_day   = __bootinfo->_SEBMinorVersion ^ ((__bootinfo->_SEBMinorVersion >> 8) << 8);
+    uint8_t _seb_minver_month = (__bootinfo->_SEBMinorVersion ^ _seb_minver_day) >> 8;
 
     uint64_t _mem_size,
              _usable_mem,
@@ -35,9 +35,9 @@ void kernel_main(boot_t __bootinfo) {
     uint64_t _kernel_size  = (uint64_t)(&_KERNEL_END) - (uint64_t)(&_KERNEL_START);
     uint64_t _kernel_pages = (uint64_t)(_kernel_size) / 4096 + 1;
 
-    kernel_kutils_kdd_setup(__bootinfo);
+    kernel_kutils_kdd_setup(*__bootinfo);
     kernel_kutils_gdt_setup();
-    kernel_kutils_mem_setup(__bootinfo);
+    kernel_kutils_mem_setup(*__bootinfo);
     kernel_kutils_int_setup();
 
     acpiinfo_t _acpi = kernel_kutils_rsd_setup(__bootinfo);
@@ -54,16 +54,16 @@ void kernel_main(boot_t __bootinfo) {
     );
 
     kprintf("Copyright 2021 - 2022 Alessandro Salerno. All rights reserved.\n");
-    kprintf("SalernOS EFI Bootloader %u-%c%u\n", __bootinfo._SEBMajorVersion, _seb_minver_month, _seb_minver_day);
+    kprintf("SalernOS EFI Bootloader %u-%c%u\n", __bootinfo->_SEBMajorVersion, _seb_minver_month, _seb_minver_day);
     kprintf("SalernOS Kernel DEV (Rome)\n\n");
 
     kprintf("Kernel Base: %u\n", (uint64_t)(&_KERNEL_START));
     kprintf("Kernel End: %u\n", (uint64_t)(&_KERNEL_END));
     kprintf("Kernel Size: %u bytes (%u pages)\n\n", _kernel_size, _kernel_pages);
 
-    kprintf("Framebuffer Resolution: %u x %u\n", __bootinfo._Framebuffer._PixelsPerScanLine, __bootinfo._Framebuffer._Height);
-    kprintf("Framebuffer Base: %u\n",            __bootinfo._Framebuffer._BaseAddress);
-    kprintf("Framebuffer Size: %u bytes\n\n",    __bootinfo._Framebuffer._BufferSize);
+    kprintf("Framebuffer Resolution: %u x %u\n", __bootinfo->_Framebuffer._PixelsPerScanLine, __bootinfo->_Framebuffer._Height);
+    kprintf("Framebuffer Base: %u\n",            __bootinfo->_Framebuffer._BaseAddress);
+    kprintf("Framebuffer Size: %u bytes\n\n",    __bootinfo->_Framebuffer._BufferSize);
 
     kernel_pgfa_info_get(&_free_mem, &_used_mem, &_unusable_mem);
     kprintf("System Memory: %u bytes\n",   _mem_size);
@@ -73,7 +73,7 @@ void kernel_main(boot_t __bootinfo) {
     kprintf("Reserved Memory: %u bytes\n", _unusable_mem);
     kprintf("Heap Test: %u\n\n",           (uint64_t)(_malloc_test));
 
-    kprintf("RSDP Address: %u\n",   (uint64_t)(__bootinfo._RSDP));
+    kprintf("RSDP Address: %u\n",   (uint64_t)(__bootinfo->_RSDP));
     kprintf("MCFG Address: %u\n\n", (uint64_t)(_acpi._MCFG));
 
     kernel_hw_pci_enumerate(_acpi._MCFG);
