9f85378733e8b01caf79267bd2cdac012cdbd9cb
tzyvoski <67583725+tzyvoski@users.noreply.github.com>
2022-04-02 19:58:41 +0200

    First steps towards AHCI Driver

---
+++ src/Hardware/Drivers/AHCI/ahci.c
@@ -0,0 +1,64 @@
+/**********************************************************************
+SalernOS Kernel
+Copyright (C) 2021 - 2022 Alessandro Salerno
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**********************************************************************/
+
+
+#include "Hardware/Drivers/AHCI/ahci.h"
+#include "kernelpanic.h"
+#include <kstdio.h>
+
+#define MAX_PORTS 32
+
+#define PORT_PRESENT 0x03
+#define PORT_ACTIVE  0x01
+
+#define SIG_ATAPI 0xEB140101
+#define SIG_ATA   0x00000101
+#define SIG_SEMB  0xC33C0101
+#define SIG_PM    0x96690101
+
+
+static ahciport_t __check_port__(hbaport_t* __port) {
+    uint32_t _status = __port->_SATAStatus;
+    
+    uint8_t _intf_power_management = (_status >> 8) & 0b111;
+    uint8_t _dev_detection         = _status & 0b111;
+
+    SOFTASSERT(_dev_detection == PORT_PRESENT, AHCI_PORT_NONE);
+    SOFTASSERT(_intf_power_management == PORT_ACTIVE, AHCI_PORT_NONE);
+    
+    switch (__port->_Signature) {
+        case SIG_ATAPI  : return AHCI_PORT_SATAPI;
+        case SIG_ATA    : return AHCI_PORT_SATA;
+        case SIG_PM     : return AHCI_PORT_PM;
+        case SIG_SEMB   : return AHCI_PORT_SEMB;
+    }
+
+    return AHCI_PORT_NONE;
+}
+
+
+void kernel_hw_ahci_ports_probe(ahcidevdr_t* __dev) {
+    uint32_t _ports_impl = __dev->_ABAR->_PortsImpplemented;
+
+    for (uint32_t _i = 0; _i < MAX_PORTS; _i++) {
+        if (_ports_impl & (1 << _i)) {
+            ahciport_t _ptype = __check_port__(&__dev->_ABAR->_HBAPorts[_i]);
+        }
+    }
+}
---
+++ src/Hardware/Drivers/AHCI/ahci.h
@@ -0,0 +1,88 @@
+/**********************************************************************
+SalernOS Kernel
+Copyright (C) 2021 - 2022 Alessandro Salerno
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**********************************************************************/
+
+
+#ifndef SALERNOS_CORE_KERNEL_HW_DRIVERS_AHCI
+#define SALERNOS_CORE_KERNEL_HW_DRIVERS_AHCI
+
+    #include <kerntypes.h>
+    #include "Hardware/PCI/pci.h"
+
+
+    typedef enum AHCIPortTypes {
+        AHCI_PORT_NONE,
+        AHCI_PORT_SATA,
+        AHCI_PORT_SEMB,
+        AHCI_PORT_PM,
+        AHCI_PORT_SATAPI
+    } ahciport_t;
+
+    typedef struct HBAPort {
+        uint32_t _CommandListBase;
+        uint32_t _CommandListBaseUpper;
+        uint32_t _FisBaseAddress;
+        uint32_t _FisBaseAddressUpper;
+        uint32_t _InterruptStatus;
+        uint32_t _InterruptEnable;
+        uint32_t _CommandStatu;
+        uint32_t _ReservedZero;
+        uint32_t _TaskFileData;
+        uint32_t _Signature;
+        uint32_t _SATAStatus;
+        uint32_t _SATAControl;
+        uint32_t _SATAError;
+        uint32_t _SATAActive;
+        uint32_t _CommandIssue;
+        uint32_t _SATANotification;
+        uint32_t _FisSwitchControl;
+        
+        uint32_t _Reserved[11];
+        uint32_t _VendorSpecific[4];
+    } hbaport_t;
+
+    typedef struct HBAMemory {
+        uint32_t  _HostCapability;
+        uint32_t  _GLobalHostControl;
+        uint32_t  _InterruptStatus;
+        uint32_t  _PortsImpplemented;
+        uint32_t  _Version;
+        uint32_t  _CCCControl;
+        uint32_t  _CCCPorts;
+        uint32_t  _EnclosureManagementLocation;
+        uint32_t  _EnclosureManagementControl;
+        uint32_t  _HostCapabilitiesExt;
+        uint32_t  _BIOSHandoff;
+
+        uint8_t   _Reserved[116];
+        uint8_t   _VendorSpecific[96];
+        hbaport_t _HBAPorts[1];
+    } hbamem_t;
+
+    typedef struct AHCIDeviceDriver {
+        pcidevhdr_t* _PCIDeviceBase;
+        hbamem_t*    _ABAR;
+    } ahcidevdr_t;
+
+
+    /*****************************************************************
+    RET TYPE        FUNCTION NAME                 FUNCTION ARGUMENTS
+    ******************************************************************/
+    void            kernel_hw_ahci_ports_probe    (ahcidevdr_t* __dev);
+
+#endif
