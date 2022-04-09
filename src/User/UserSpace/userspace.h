152e022be1dcefcf81ac2d0c093f314123bc8acd
tzyvoski <OxyGen05@outlook.it>
2022-04-09 09:30:09 +0200

    Added experimental support for Userspace

---
+++ src/User/UserSpace/userspace.h
@@ -0,0 +1,30 @@
+/**********************************************************************
+SalernOS Kernel
+Copyright (C) 2021 - 2022 Alessandro Salerno
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**********************************************************************/
+
+
+#ifndef SALERNOS_CORE_KERNEL_USER_USERSPACE
+#define SALERNOS_CORE_KERNEL_USER_USERSPACE
+
+    /**********************************************************************************
+    RET TYPE        FUNCTION NAME                 FUNCTION ARGUMENTS
+    **********************************************************************************/
+    void            kernel_userspace_enter        (void* __usrentry, void* __usrstack);
+    void            kernel_userspace_exit         ();
+
+#endif
