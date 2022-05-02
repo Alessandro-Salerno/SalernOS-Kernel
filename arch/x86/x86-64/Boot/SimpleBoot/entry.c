37b792af4901df27546bda8e7697d17cce3dd4c5
tzyvoski <67583725+tzyvoski@users.noreply.github.com>
2022-05-02 14:32:50 +0200

    Moved some more stuff where it should be - probably brke everything

---
+++ arch/x86/x86-64/Boot/SimpleBoot/entry.c
@@ -0,0 +1,73 @@
+/**********************************************************************
+SalernOS Kernel
+Copyright (C) 2021 - 2022 Alessandro Salerno
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**********************************************************************/
+
+
+#include <kerninc.h>
+#include <kdebug.h>
+
+
+void kernel_entry(boot_t* __bootinfo) {
+    uint64_t _mem_size,
+             _usable_mem,
+             _free_mem,
+             _used_mem,
+             _unusable_mem;
+
+    uint64_t _kernel_size  = (uint64_t)(&_KERNEL_END) - (uint64_t)(&_KERNEL_START);
+    uint64_t _kernel_pages = (uint64_t)(_kernel_size) / 4096 + 1;
+
+    kernel_kutils_kdd_setup(__bootinfo);
+    kernel_kutils_gdt_setup();
+    kernel_kutils_mem_setup(__bootinfo);
+    kernel_kutils_sc_setup();
+    kernel_kutils_int_setup();
+    kernel_kutils_time_setup();
+
+    acpiinfo_t _acpi = kernel_kutils_rsd_setup(__bootinfo);
+
+    kernel_mmap_info_get(&_mem_size, &_usable_mem, NULL, NULL);
+    kprintf("\n\nCopyright 2021 - 2022 Alessandro Salerno. All rights reserved.\n");
+    kprintf("%s %s\n", __bootinfo->_BootloaderName, __bootinfo->_BootloaderVersion);
+    kprintf("SalernOS Kernel DEV (Vienna)\n\n");
+
+    kprintf("Kernel Base: %u\n", (uint64_t)(&_KERNEL_START));
+    kprintf("Kernel End: %u\n", (uint64_t)(&_KERNEL_END));
+    kprintf("Kernel Size: %u bytes (%u pages)\n\n", _kernel_size, _kernel_pages);
+
+    kprintf("Framebuffer Resolution: %u x %u\n", __bootinfo->_Framebuffer._Width, __bootinfo->_Framebuffer._Height);
+    kprintf("Framebuffer Base: %u\n",            __bootinfo->_Framebuffer._BaseAddress);
+    kprintf("Framebuffer Size: %u bytes\n\n",    __bootinfo->_Framebuffer._BufferSize);
+
+    kernel_pgfa_info_get(&_free_mem, &_used_mem, &_unusable_mem);
+    kprintf("System Memory: %u bytes\n",     _mem_size);
+    kprintf("Usable Memory: %u bytes\n",     _usable_mem);
+    kprintf("Free Memory: %u bytes\n",       _free_mem);
+    kprintf("Used Memory: %u bytes\n",       _used_mem);
+    kprintf("Reserved Memory: %u bytes\n\n", _unusable_mem);
+
+    kprintf("RSDP Address: %u\n",   (uint64_t)(__bootinfo->_RSDP));
+    kprintf("MCFG Address: %u\n", (uint64_t)(_acpi._MCFG));
+
+    klogok("Kernel Ready!");
+    kprintf("\n\n");
+
+    while (TRUE) {
+        asm volatile ("hlt");
+    }
+}
--- src/kernel.c
+++ src/kernel.c
@@ -22,51 +22,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <kdebug.h>
 
 
-void kernel_main(boot_t* __bootinfo) {
-    uint64_t _mem_size,
-             _usable_mem,
-             _free_mem,
-             _used_mem,
-             _unusable_mem;
-
-    uint64_t _kernel_size  = (uint64_t)(&_KERNEL_END) - (uint64_t)(&_KERNEL_START);
-    uint64_t _kernel_pages = (uint64_t)(_kernel_size) / 4096 + 1;
-
-    kernel_kutils_kdd_setup(__bootinfo);
-    kernel_kutils_gdt_setup();
-    kernel_kutils_mem_setup(__bootinfo);
-    kernel_kutils_sc_setup();
-    kernel_kutils_int_setup();
-    kernel_kutils_time_setup();
-
-    acpiinfo_t _acpi = kernel_kutils_rsd_setup(__bootinfo);
-    
-    kernel_mmap_info_get(&_mem_size, &_usable_mem, NULL, NULL);
-    kprintf("\n\nCopyright 2021 - 2022 Alessandro Salerno. All rights reserved.\n");
-    kprintf("%s %s\n", __bootinfo->_BootloaderName, __bootinfo->_BootloaderVersion);
-    kprintf("SalernOS Kernel DEV (Vienna)\n\n");
-
-    kprintf("Kernel Base: %u\n", (uint64_t)(&_KERNEL_START));
-    kprintf("Kernel End: %u\n", (uint64_t)(&_KERNEL_END));
-    kprintf("Kernel Size: %u bytes (%u pages)\n\n", _kernel_size, _kernel_pages);
-
-    kprintf("Framebuffer Resolution: %u x %u\n", __bootinfo->_Framebuffer._Width, __bootinfo->_Framebuffer._Height);
-    kprintf("Framebuffer Base: %u\n",            __bootinfo->_Framebuffer._BaseAddress);
-    kprintf("Framebuffer Size: %u bytes\n\n",    __bootinfo->_Framebuffer._BufferSize);
-
-    kernel_pgfa_info_get(&_free_mem, &_used_mem, &_unusable_mem);
-    kprintf("System Memory: %u bytes\n",     _mem_size);
-    kprintf("Usable Memory: %u bytes\n",     _usable_mem);
-    kprintf("Free Memory: %u bytes\n",       _free_mem);
-    kprintf("Used Memory: %u bytes\n",       _used_mem);
-    kprintf("Reserved Memory: %u bytes\n\n", _unusable_mem);
-
-    kprintf("RSDP Address: %u\n",   (uint64_t)(__bootinfo->_RSDP));
-    kprintf("MCFG Address: %u\n", (uint64_t)(_acpi._MCFG));
-
-    klogok("Kernel Ready!");
-    kprintf("\n\n");
-
+void kernel_main() {
     while (TRUE) {
         asm volatile ("hlt");
     }
