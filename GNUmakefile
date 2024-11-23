c59fd8802e392842b1cf3e3e2a4c40cddca86427
Alessandro-Salerno <oxygen05@outlook.it>
2024-11-23 17:56:41 +0100

    wiki limine bare bones

---
+++ GNUmakefile
@@ -0,0 +1,105 @@
+# Nuke built-in rules and variables.
+MAKEFLAGS += -rR
+.SUFFIXES:
+
+# This is the name that our final executable will have.
+# Change as needed.
+override OUTPUT := myos
+
+# User controllable C compiler command.
+CC := cc
+
+# User controllable linker command.
+LD := ld
+
+# User controllable C flags.
+CFLAGS := -g -O2 -pipe
+
+# User controllable C preprocessor flags. We set none by default.
+CPPFLAGS :=
+
+# User controllable nasm flags.
+NASMFLAGS := -F dwarf -g
+
+# User controllable linker flags. We set none by default.
+LDFLAGS :=
+
+# Internal C flags that should not be changed by the user.
+override CFLAGS += \
+    -Wall \
+    -Wextra \
+    -std=gnu11 \
+    -ffreestanding \
+    -fno-stack-protector \
+    -fno-stack-check \
+    -fno-lto \
+    -fno-PIC \
+    -m64 \
+    -march=x86-64 \
+    -mno-80387 \
+    -mno-mmx \
+    -mno-sse \
+    -mno-sse2 \
+    -mno-red-zone \
+    -mcmodel=kernel
+
+# Internal C preprocessor flags that should not be changed by the user.
+override CPPFLAGS := \
+    -I src \
+    $(CPPFLAGS) \
+    -MMD \
+    -MP
+
+# Internal nasm flags that should not be changed by the user.
+override NASMFLAGS += \
+    -Wall \
+    -f elf64
+
+# Internal linker flags that should not be changed by the user.
+override LDFLAGS += \
+    -m elf_x86_64 \
+    -nostdlib \
+    -static \
+    -z max-page-size=0x1000 \
+    -T linker.ld
+
+# Use "find" to glob all *.c, *.S, and *.asm files in the tree and obtain the
+# object and header dependency file names.
+override CFILES := $(shell cd src && find -L * -type f -name '*.c' | LC_ALL=C sort)
+override ASFILES := $(shell cd src && find -L * -type f -name '*.S' | LC_ALL=C sort)
+override NASMFILES := $(shell cd src && find -L * -type f -name '*.asm' | LC_ALL=C sort)
+override OBJ := $(addprefix obj/,$(CFILES:.c=.c.o) $(ASFILES:.S=.S.o) $(NASMFILES:.asm=.asm.o))
+override HEADER_DEPS := $(addprefix obj/,$(CFILES:.c=.c.d) $(ASFILES:.S=.S.d))
+
+# Default target.
+.PHONY: all
+all: bin/$(OUTPUT)
+
+# Link rules for the final executable.
+bin/$(OUTPUT): GNUmakefile linker.ld $(OBJ)
+	mkdir -p "$$(dirname $@)"
+	$(LD) $(OBJ) $(LDFLAGS) -o $@
+
+# Include header dependencies.
+-include $(HEADER_DEPS)
+
+# Compilation rules for *.c files.
+obj/%.c.o: src/%.c GNUmakefile
+	mkdir -p "$$(dirname $@)"
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
+
+# Compilation rules for *.S files.
+obj/%.S.o: src/%.S GNUmakefile
+	mkdir -p "$$(dirname $@)"
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
+
+# Compilation rules for *.asm (nasm) files.
+obj/%.asm.o: src/%.asm GNUmakefile
+	mkdir -p "$$(dirname $@)"
+	nasm $(NASMFLAGS) $< -o $@
+
+# Remove object files and the final executable.
+.PHONY: clean
+clean:
+	rm -rf bin obj
+
---
+++ src/main.c
@@ -0,0 +1,128 @@
+#include <limine.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// Set the base revision to 3, this is recommended as this is the latest
+// base revision described by the Limine boot protocol specification.
+// See specification for further info.
+
+__attribute__((
+    used,
+    section(".limine_requests"))) static volatile LIMINE_BASE_REVISION(3);
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent, _and_ they should be accessed at least
+// once or marked as used with the "used" attribute as done here.
+
+__attribute__((
+    used,
+    section(
+        ".limine_requests"))) static volatile struct limine_framebuffer_request
+    framebuffer_request = {.id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 0};
+
+// Finally, define the start and end markers for the Limine requests.
+// These can also be moved anywhere, to any .c file, as seen fit.
+
+__attribute__((used,
+               section(".limine_requests_"
+                       "start"))) static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((
+    used,
+    section(
+        ".limine_requests_end"))) static volatile LIMINE_REQUESTS_END_MARKER;
+
+// GCC and Clang reserve the right to generate calls to the following
+// 4 functions even if they are not directly called.
+// Implement them as the C specification mandates.
+// DO NOT remove or rename these functions, or stuff will eventually break!
+// They CAN be moved to a different .c file.
+
+void *memcpy(void *dest, const void *src, size_t n) {
+  uint8_t       *pdest = (uint8_t *)dest;
+  const uint8_t *psrc  = (const uint8_t *)src;
+
+  for (size_t i = 0; i < n; i++) {
+    pdest[i] = psrc[i];
+  }
+
+  return dest;
+}
+
+void *memset(void *s, int c, size_t n) {
+  uint8_t *p = (uint8_t *)s;
+
+  for (size_t i = 0; i < n; i++) {
+    p[i] = (uint8_t)c;
+  }
+
+  return s;
+}
+
+void *memmove(void *dest, const void *src, size_t n) {
+  uint8_t       *pdest = (uint8_t *)dest;
+  const uint8_t *psrc  = (const uint8_t *)src;
+
+  if (src > dest) {
+    for (size_t i = 0; i < n; i++) {
+      pdest[i] = psrc[i];
+    }
+  } else if (src < dest) {
+    for (size_t i = n; i > 0; i--) {
+      pdest[i - 1] = psrc[i - 1];
+    }
+  }
+
+  return dest;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+  const uint8_t *p1 = (const uint8_t *)s1;
+  const uint8_t *p2 = (const uint8_t *)s2;
+
+  for (size_t i = 0; i < n; i++) {
+    if (p1[i] != p2[i]) {
+      return p1[i] < p2[i] ? -1 : 1;
+    }
+  }
+
+  return 0;
+}
+
+// Halt and catch fire function.
+static void hcf(void) {
+  for (;;) {
+    asm("hlt");
+  }
+}
+
+// The following will be our kernel's entry point.
+// If renaming kmain() to something else, make sure to change the
+// linker script accordingly.
+void kmain(void) {
+  // Ensure the bootloader actually understands our base revision (see spec).
+  if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+    hcf();
+  }
+
+  // Ensure we got a framebuffer.
+  if (framebuffer_request.response == NULL ||
+      framebuffer_request.response->framebuffer_count < 1) {
+    hcf();
+  }
+
+  // Fetch the first framebuffer.
+  struct limine_framebuffer *framebuffer =
+      framebuffer_request.response->framebuffers[0];
+
+  // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+  for (size_t i = 0; i < 100; i++) {
+    volatile uint32_t *fb_ptr                = framebuffer->address;
+    fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+  }
+
+  // We're done, just hang...
+  hcf();
+}
